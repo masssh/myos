6d7dbb25b4feb6bf8d1ba23186e20484b43ab6ad
masssh <utm.ver.2.0@gmail.com>
2019-09-15 14:30:34 +0900

    Fix bootloader

--- src/bootloader.s
+++ src/bootloader.s
@@ -1,34 +1,33 @@
-        bits 16
-        org 0x7c00
+		bits 16 ; real mode
+		org 0x7c00 ; entry address for BIOS bootstrap
 start:
-        xor     ax,     ax
-        mov     ds,     ax
-        mov     es,     ax
-        mov     fs,     ax
-        mov     gs,     ax
-        mov     ss,     ax
-        mov     sp,     0x7c00
+		; initialize segment registers
+		xor     ax, ax
+		mov     ds, ax
+		mov     es, ax
+		mov     fs, ax
+		mov     gs, ax
+		mov     ss, ax
+		
+		mov     sp, 0x7c00 ; initialize stack pointer
+		mov		si, msg
 
-        mov     al,     0x00
-        mov     ah,     0x03
-        int     0x10
+putloop:
+		; print message
+		mov		al,[si]
+		add		si,1
+		cmp		al,0
+		je		fin
+		mov		ah,0x0e
+		mov		bx,15
+		int		0x10
+		jmp		putloop
 
-        mov     si,     msg
-        mov     ah,     0x0e
-
-println:
-        lodsb
-        or      al,     al
-        jz      hang
-        
-        int     0x10
-        jmp     println
-
-hang:
-        hlt
-        jmp     hang
+fin:
+		hlt
+		jmp     fin
 
 msg:
-        db      "Hello, World", 13, 10, 0
-        times   510 - ($-$$)    db      0
-        db      0x55, 0xaa
+		db      "Hello, World", 13, 10, 0
+		times   510-($-$$) db 0
+		db      0x55, 0xaa
